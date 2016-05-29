f0c1a5313e15190e5f5998030f8f4bbb3ebafb20
indocreator <portsign@gmail.com>
2016-05-29 12:04:54 +0700

    adding google analytics

--- header.php
+++ header.php
@@ -5,6 +5,14 @@
 	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title> 
 	<?php wp_head(); ?>
 	<meta name="google-site-verification" content="hKuOXpc3ioyP3K8wws9XvCSGm2nFdGlS6gHKM_Bodpk" />
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-78474393-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,200' rel='stylesheet' type='text/css' />
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
