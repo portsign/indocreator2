ab295fe9e6e442599e6a77c94e93f48df1e10fb7
indocreator <portsign@gmail.com>
2016-06-13 23:58:05 +0700

    added facebook comment

--- single.php
+++ single.php
@@ -30,6 +30,24 @@
 					
 					<br /><br />
 					
+					<script>
+					  window.fbAsyncInit = function() {
+					    FB.init({
+					      appId      : '104501219977666',
+					      xfbml      : true,
+					      version    : 'v2.6'
+					    });
+					  };
+
+					  (function(d, s, id){
+					     var js, fjs = d.getElementsByTagName(s)[0];
+					     if (d.getElementById(id)) {return;}
+					     js = d.createElement(s); js.id = id;
+					     js.src = "//connect.facebook.net/en_US/sdk.js";
+					     fjs.parentNode.insertBefore(js, fjs);
+					   }(document, 'script', 'facebook-jssdk'));
+					</script>
+
 												
 				
 				<?php endwhile; else: ?>
